52575e5724f19f631c6745c600035266500e5b1b
namvt3 <vunamdhhl@gmail.com>
2023-10-26 14:38:02 +0700

    fix

--- src/components/content/problem3/index.jsx
+++ src/components/content/problem3/index.jsx
@@ -1,4 +1,5 @@
 import FormProblem3 from "./form";
+import { CodeBlock } from "react-code-blocks";
 
 export default function Problem3() {
   const answer = [
@@ -127,8 +128,6 @@ export default function Problem3() {
     },
   ];
 
-  const link = "https://github.com/VuTienNam/VuTienNam99TechTest";
-
   return (
     <div className='problem3'>
       {answer.map((item, index) => (
@@ -140,18 +139,79 @@ export default function Problem3() {
           index={index}
         />
       ))}
-      <p>
-        Full code is in this repo:{" "}
-        <a
-          onClick={(e) => {
-            e.preventDefault();
-            window.open(link);
-          }}
-          href={link}
-        >
-          {link}
-        </a>
-      </p>
+
+      <p>Full refactored code: </p>
+      <CodeBlock
+        text={`interface WalletBalance {
+  currency: string;
+  amount: number;
+}
+interface FormattedWalletBalance extends WalletBalance {
+  formatted: string;
+}
+
+interface Props extends BoxProps {}
+const WalletPage: React.FC<Props> = (props: Props) => {
+  const { children, ...rest } = props;
+  const balances = useWalletBalances();
+  const prices = usePrices();
+
+  const getPriority = (blockchain: string): number => {
+    const myObjectLookup = {
+      Osmosis: 100,
+      Ethereum: 50,
+      Arbitrum: 30,
+      Zilliqa: 20,
+      Neo: 20,
+    };
+    const defaultValue = -99;
+    return myObjectLookup[blockchain] || defaultValue;
+  };
+
+  const sortedBalances = useMemo(() => {
+    const filtered = balances.filter((balance: WalletBalance) => {
+      const balancePriority = getPriority(balance.blockchain);
+      if (balancePriority > -99 && balance.amount <= 0) {
+        return true;
+      }
+      return false;
+    });
+
+    const filteredAndSorted = filtered.sort((lhs: WalletBalance, rhs: WalletBalance) => {
+      const leftPriority = getPriority(lhs.blockchain);
+      const rightPriority = getPriority(rhs.blockchain);
+      return rightPriority - leftPriority;
+    });
+
+    return filteredAndSorted;
+  }, [balances, prices]);
+
+  const formattedBalances = sortedBalances.map((balance: WalletBalance) => {
+    return {
+      ...balance,
+      formatted: balance.amount.toFixed(),
+    };
+  });
+
+  const rows = formattedBalances.map((balance: FormattedWalletBalance, index: number) => {
+    const usdValue = prices[balance.currency] * balance.amount;
+    return (
+      <WalletRow
+        className={classes.row}
+        key={index}
+        amount={balance.amount}
+        usdValue={usdValue}
+        formattedAmount={balance.formatted}
+      />
+    );
+  });
+
+  return <div {...rest}>{rows}</div>;
+};`}
+        language='js'
+        showLineNumbers
+        wrapLines
+      />
     </div>
   );
 }
